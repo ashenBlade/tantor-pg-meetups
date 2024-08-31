4f2b43f8873771891daa348132dac22f18183d93
Sergey Solovev <ashen.blade@ya.ru>
2024-08-31 16:50:34 +0300

    добавил EXPLAIN ANALYZE к запросам

--- PGBootcamp2024/debugging-postgres-planner/repo/queries_constraint_exclusion.sql
+++ PGBootcamp2024/debugging-postgres-planner/repo/queries_constraint_exclusion.sql
@@ -17,20 +17,20 @@ ANALYZE tbl2;
 
 -- Queries for Constraint Exclusion tests
 
-SELECT * FROM tbl1 WHERE value > 0 AND value <= 0;
+EXPLAIN ANALYZE SELECT * FROM tbl1 WHERE value > 0 AND value <= 0;
 
-SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.value > 0 WHERE t1.value <= 0;
+EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.value > 0 WHERE t1.value <= 0;
 
 -- More complex queries
-SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.id = t2.id AND t1.value > 0 WHERE t1.value <= 0;
+EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.id = t2.id AND t1.value > 0 WHERE t1.value <= 0;
 
-SELECT * FROM tbl1 t1
+EXPLAIN ANALYZE SELECT * FROM tbl1 t1
 JOIN LATERAL (
     SELECT MAX(value) FROM tbl2 GROUP BY id WHERE t1.value > 0
 ) t2 ON t1.id = t2.id
 WHERE t1.value <= 0;
 
 -- TODO: check self-join
-SELECT * FROM tbl1 t1
+EXPLAIN ANALYZE SELECT * FROM tbl1 t1
 JOIN tbl2 t2 ON t1.id = t2.id
 WHERE t1.value > 0 AND t1.value <= 0;
