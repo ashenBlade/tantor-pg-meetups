fbaadd1e51ab7af31a8509666933416024b0c5f3
Sergey Solovev <ashen.blade@ya.ru>
2024-08-31 10:02:15 +0300

    переместил скрипты в свои директории

--- PGBootcamp2024/debugging-postgres-planner/repo/init_repo.sh
+++ PGBootcamp2024/debugging-postgres-planner/repo/init_repo.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 
+
+
 # Clone repository
 git clone https://git.postgresql.org/git/postgresql.git \
     --single-branch --branch=REL_16_4 postgresql
 
 # Setup dev scripts
-mkdir -p postgresql/dev
-cp utils.sh setup.sh build.sh run.sh clean.sh   \
-    get_current_backend_pid.sh test.sh          \
-    postgresql/dev
-chmod +x postgresql/dev/*
+cp -r dev postgresql/dev
 
 # Setup VS Code config files
-mkdir -p postgresql/.vscode
-cp *.json postgresql/.vscode
+cp -r .vscode postgresql/.vscode
+
+# Install required extensions
+code --install-extension ash-blade.postgresql-hacker-helper
+code --install-extension ms-vscode.cpptools
