9494bf506bc2ba9c7d69892d085d333717fd4535
Sergey Solovev <ashen.blade@ya.ru>
2024-09-01 14:47:48 +0300

    доработал речь

--- PGBootcamp2024/debugging-postgres-planner/repo/queries_constrexcl.sql
+++ PGBootcamp2024/debugging-postgres-planner/repo/queries_constrexcl.sql
@@ -5,15 +5,18 @@
 -- Queries for Constraint Exclusion tests
 EXPLAIN ANALYZE SELECT * FROM tbl1 WHERE value > 0 AND value <= 0;
 
-EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.value > 0 WHERE t1.value <= 0;
+EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 
+ON t1.value > 0 
+WHERE t1.value <= 0;
 
 
 ----------------------
 -- More complex queries
 EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.id = t2.id AND t1.value > 0 WHERE t1.value <= 0;
 
+-- TODO: remove
 EXPLAIN ANALYZE SELECT * FROM tbl1 t1
 JOIN LATERAL (
-    SELECT MAX(value) FROM tbl2 GROUP BY id WHERE t1.value > 0
+    SELECT MAX(value) as id FROM tbl2 WHERE t1.value > 0 GROUP BY id
 ) t2 ON t1.id = t2.id
 WHERE t1.value <= 0;
