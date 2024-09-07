3bfe4488df4d9fa684dae08490cda3c234d6b4b5
Sergey Solovev <ashen.blade@ya.ru>
2024-09-07 16:39:12 +0300

    еще доработал

--- PGBootcamp2024/debugging-postgres-planner/repo/queries_constrexcl.sql
+++ PGBootcamp2024/debugging-postgres-planner/repo/queries_constrexcl.sql
@@ -1,22 +1,21 @@
--- Schema in `schema_constrexpr.sql`
-----------------------
+-- Schema `schema_constrexpr.sql`
+
+EXPLAIN ANALYZE 
+SELECT * FROM tbl1 
+WHERE value > 0 
+      AND 
+      value <= 0;
 
 
--- Queries for Constraint Exclusion tests
-EXPLAIN ANALYZE SELECT * FROM tbl1 WHERE value > 0 AND value <= 0;
-
-EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 
-ON t1.value > 0 
+EXPLAIN ANALYZE 
+SELECT * FROM tbl1 t1 
+JOIN tbl2 t2 
+   ON t1.value > 0 
 WHERE t1.value <= 0;
 
 
-----------------------
--- More complex queries
-EXPLAIN ANALYZE SELECT * FROM tbl1 t1 JOIN tbl2 t2 ON t1.id = t2.id AND t1.value > 0 WHERE t1.value <= 0;
-
--- TODO: remove
-EXPLAIN ANALYZE SELECT * FROM tbl1 t1
-JOIN LATERAL (
-    SELECT MAX(value) as id FROM tbl2 WHERE t1.value > 0 GROUP BY id
-) t2 ON t1.id = t2.id
-WHERE t1.value <= 0;
+EXPLAIN ANALYZE
+SELECT * FROM generate_series(1, 100) g(value)
+WHERE value > 0
+      AND
+      value <= 0;
